86f0a9848f5f214a58c7aff42542912426e24a57
Lars Francke <lars.francke@gmail.com>
2021-08-03 13:14:42 +0200

    Fix clippy warnings in 1.54

--- crd/src/error.rs
+++ crd/src/error.rs
@@ -1,3 +1,4 @@
+#[allow(clippy::enum_variant_names)]
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("Pod has no hostname assignment, this is most probably a transient failure and should be retried: [{pod}]")]
