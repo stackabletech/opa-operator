c972a0ed7791aa66ba9bc87c5c7daa1976962521
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2021-09-28 12:01:35 +0200

    Update with changes from operator-rs.

--- rust/operator/src/lib.rs
+++ rust/operator/src/lib.rs
@@ -171,9 +171,12 @@ impl OpaState {
                         &self.existing_pods,
                     )?;
 
-                    let mapping = state
-                        .remaining_mapping()
-                        .get_filtered(&role_str.to_string(), role_group);
+                    let mapping = state.remaining_mapping().filter(
+                        APP_NAME,
+                        &self.context.name(),
+                        &role_str.to_string(),
+                        role_group,
+                    );
 
                     if let Some((pod_id, node_id)) = mapping.iter().next() {
                         // now we have a node that needs a pod -> get validated config
