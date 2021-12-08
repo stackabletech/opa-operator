67d813060f51c55b2474ccccce9b1cceb4c3b982
Teo Klestrup Röijezon <teo@nullable.se>
2021-12-08 10:26:20 +0100

    Set up correct controller watches

--- rust/operator/src/lib.rs
+++ rust/operator/src/lib.rs
@@ -6,7 +6,8 @@ use futures::StreamExt;
 use stackable_opa_crd::{OpenPolicyAgent, APP_NAME};
 use stackable_operator::client::Client;
 use stackable_operator::error::OperatorResult;
-use stackable_operator::k8s_openapi::api::core::v1::{ConfigMap, Pod};
+use stackable_operator::k8s_openapi::api::apps::v1::DaemonSet;
+use stackable_operator::k8s_openapi::api::core::v1::{ConfigMap, Service};
 use stackable_operator::kube::api::ListParams;
 use stackable_operator::kube::runtime::controller::Context;
 use stackable_operator::kube::runtime::Controller;
@@ -18,12 +19,14 @@ use stackable_operator::product_config::ProductConfigManager;
 /// This is an async method and the returned future needs to be consumed to make progress.
 pub async fn create_controller(client: Client, product_config_path: &str) -> OperatorResult<()> {
     let opa_api: Api<OpenPolicyAgent> = client.get_all_api();
-    let pods_api: Api<Pod> = client.get_all_api();
+    let daemonsets_api: Api<DaemonSet> = client.get_all_api();
     let configmaps_api: Api<ConfigMap> = client.get_all_api();
+    let services_api: Api<Service> = client.get_all_api();
 
     let controller = Controller::new(opa_api, ListParams::default())
-        .owns(pods_api, ListParams::default())
-        .owns(configmaps_api, ListParams::default());
+        .owns(daemonsets_api, ListParams::default())
+        .owns(configmaps_api, ListParams::default())
+        .owns(services_api, ListParams::default());
 
     let product_config = ProductConfigManager::from_yaml_file(product_config_path).unwrap();
 
