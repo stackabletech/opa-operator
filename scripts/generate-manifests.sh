ec5c2fe1ad79e71fc602e9cb9736bd912d0996f9
Stacky McStackface <github@stackable.de>
2022-02-07 19:19:14 +0000

    Generated commit to update templated files based on rev b5ebd49 in stackabletech/operator-templating repo.
    Original commit message:
    Publishing Artifacts step is skipped for bors staging branches but marked as required. (#63)
    
    This keeps bors from merging any prs.

--- scripts/generate-manifests.sh
+++ scripts/generate-manifests.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # This script reads a Helm chart from deploy/helm/opa-operator and
 # generates manifest files into deploy/manifestss
 set -e
 
 tmp=$(mktemp -d ./manifests-XXXXX)
 
-helm template --output-dir $tmp \
+helm template --output-dir "$tmp" \
               --include-crds \
               --name-template opa-operator \
               deploy/helm/opa-operator
 
-for file in $(find $tmp -type f)
+while IFS= read -r -d '' file
 do
-    yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' $file
-    yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' $file
-    sed -i '/# Source: .*/d' $file
-done
+    yq eval -i 'del(.. | select(has("app.kubernetes.io/managed-by")) | ."app.kubernetes.io/managed-by")' "$file"
+    yq eval -i 'del(.. | select(has("helm.sh/chart")) | ."helm.sh/chart")' "$file"
+    sed -i '/# Source: .*/d' "$file"
+done <   <(find "$tmp" -type f)
 
-cp -r $tmp/opa-operator/*/* deploy/manifests/
+cp -r "$tmp"/opa-operator/*/* deploy/manifests/
 
-rm -rf $tmp
+rm -rf "$tmp"
