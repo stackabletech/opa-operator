2c52fd70519c944e71e477d488e96c8477e6ab6f
Razvan-Daniel Mihai <84674+razvan@users.noreply.github.com>
2022-02-24 18:09:53 +0100

    Rego reference is really optional.

--- rust/crd/src/lib.rs
+++ rust/crd/src/lib.rs
@@ -5,7 +5,6 @@ use stackable_operator::role_utils::Role;
 use stackable_operator::schemars::{self, JsonSchema};
 use std::collections::BTreeMap;
 use strum_macros::EnumIter;
-use tracing::error;
 
 pub const APP_NAME: &str = "opa";
 pub const CONFIG_FILE: &str = "config.yaml";
@@ -70,11 +69,6 @@ impl Configuration for OpaConfig {
             if let Some(rego) = &self.rego_rule_reference {
                 config.insert(REGO_RULE_REFERENCE.to_string(), Some(rego.to_string()));
             }
-        } else {
-            error!(
-                "Did not find any properties matching config file [{}]. This should not happen.",
-                CONFIG_FILE
-            );
         }
 
         Ok(config)
