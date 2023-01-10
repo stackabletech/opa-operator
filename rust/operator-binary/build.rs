f47f4e2e08840caad57bd3a708ba4e50c065e995
Malte Sander <contact@maltesander.com>
2023-01-10 11:48:00 +0000

    Removed deploy crd folder (#398)
    # Description
    
    fixes https://github.com/stackabletech/opa-operator/issues/257
    
    
    
    Co-authored-by: Malte Sander <malte.sander.it@gmail.com>

--- rust/operator-binary/build.rs
+++ rust/operator-binary/build.rs
@@ -1,10 +1,3 @@
-use stackable_opa_crd::OpaCluster;
-use stackable_operator::crd::CustomResourceExt;
-
-fn main() -> Result<(), stackable_operator::error::Error> {
+fn main() {
     built::write_built_file().expect("Failed to acquire build-time information");
-
-    OpaCluster::write_yaml_schema("../../deploy/crd/opacluster.crd.yaml")?;
-
-    Ok(())
 }
