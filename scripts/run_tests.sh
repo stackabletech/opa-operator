ad4bc13df4f974aad9d525979a92e9eb7411f672
Nikolaus Winter <mail@nikolaus-winter.de>
2023-01-31 15:17:42 +0100

    no longer install Beku in the test run script

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
-set -e
 
-# Install and run beku, the tool to expand the test templates
-# (https://github.com/stackabletech/beku.py)
-pip install beku-stackabletech
+# Check if the test expansion tool beku is installed
+set +e
+which beku 2>&1 >/dev/null
+beku_installed=$?
+set -e
+if [ $beku_installed -ne 0 ]; then
+  echo "Please install beku.py to run the tests, see https://github.com/stackabletech/beku.py"
+  exit 1
+fi
+
+# Expand the tests
 beku
 
 # Run tests, pass the params
