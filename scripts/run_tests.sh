42238d2c1384bb233fe04238a7da89f8b79f2014
Nikolaus Winter <mail@nikolaus-winter.de>
2023-01-31 12:54:47 +0100

    test beku.py for OPA

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,29 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# Register absolute paths to pass to Ansible so the location of the role is irrelevant
-# for the run
-TESTDIR="$(pwd)/tests"
-WORKDIR="$(pwd)/tests/_work"
+# Install and run beku, the tool to expand the test templates
+# (https://github.com/stackabletech/beku.py)
+pip install beku-stackabletech
+beku
 
-# Create dirs
-mkdir -p tests/ansible/roles
-mkdir -p "$WORKDIR"
-
-# Install Ansible role if needed
-pushd tests/ansible
-ansible-galaxy role install -r requirements.yaml -p ./roles
-
-# TODO: create pipenv in files for script thingy
-
-# Funnel via JSON to ensure that values are escaped properly
-echo '{}' | jq '{work_dir: $WORKDIR, test_dir: $TESTDIR}' --arg WORKDIR "$WORKDIR" --arg TESTDIR "$TESTDIR" > "${WORKDIR}"/vars.json
-
-# Run playbook to generate test scenarios
-ansible-playbook playbook.yaml --extra-vars "@${WORKDIR}/vars.json"
-popd
-
-# Run tests
+# Run tests, pass the params
 pushd tests/_work
 kubectl kuttl test "$@"
 popd
