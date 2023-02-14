1a56140c6631b3f6846fc8b201077b0a89b4d209
Malte Sander <malte.sander.it@gmail.com>
2023-02-14 16:05:05 +0100

    prepare container logs

--- rust/operator-binary/src/controller.rs
+++ rust/operator-binary/src/controller.rs
@@ -1,9 +1,12 @@
 //! Ensures that `Pod`s are configured and running for each [`OpaCluster`]
 
 use crate::discovery::{self, build_discovery_configmaps};
+use crate::product_logging::{extend_role_group_config_map, resolve_vector_aggregator_address};
 
 use snafu::{OptionExt, ResultExt, Snafu};
-use stackable_opa_crd::{OpaCluster, OpaConfig, OpaRole, APP_NAME, OPERATOR_NAME};
+use stackable_opa_crd::{
+    Container, OpaCluster, OpaConfig, OpaRole, APP_NAME, OPERATOR_NAME, STACKABLE_LOG_DIR,
+};
 use stackable_operator::{
     builder::{ConfigMapBuilder, ContainerBuilder, FieldPathEnvVar, ObjectMetaBuilder, PodBuilder},
     commons::product_image_selection::ResolvedProductImage,
@@ -17,7 +20,9 @@ use stackable_operator::{
             },
             rbac::v1::{ClusterRole, RoleBinding, RoleRef, Subject},
         },
-        apimachinery::pkg::{apis::meta::v1::LabelSelector, util::intstr::IntOrString},
+        apimachinery::pkg::{
+            api::resource::Quantity, apis::meta::v1::LabelSelector, util::intstr::IntOrString,
+        },
         Resource,
     },
     kube::runtime::{controller::Action, reflector::ObjectRef},
@@ -25,6 +30,10 @@ use stackable_operator::{
     logging::controller::ReconcilerError,
     product_config::{types::PropertyNameKind, ProductConfigManager},
     product_config_utils::{transform_all_roles_to_config, validate_all_roles_and_groups_config},
+    product_logging::{
+        self,
+        spec::{ContainerLogConfig, ContainerLogConfigChoice},
+    },
     role_utils::RoleGroupRef,
 };
 use std::{
@@ -114,6 +123,20 @@ pub enum Error {
     },
     #[snafu(display("failed to resolve and merge config for role and role group"))]
     FailedToResolveConfig { source: stackable_opa_crd::Error },
+    #[snafu(display("illegal container name: [{container_name}]"))]
+    IllegalContainerName {
+        source: stackable_operator::error::Error,
+        container_name: String,
+    },
+    #[snafu(display("failed to resolve the Vector aggregator address"))]
+    ResolveVectorAggregatorAddress {
+        source: crate::product_logging::Error,
+    },
+    #[snafu(display("failed to add the logging configuration to the ConfigMap [{cm_name}]"))]
+    InvalidLoggingConfig {
+        source: crate::product_logging::Error,
+        cm_name: String,
+    },
 }
 type Result<T, E = Error> = std::result::Result<T, E>;
 
@@ -190,6 +213,10 @@ pub async fn reconcile_opa(opa: Arc<OpaCluster>, ctx: Arc<Ctx>) -> Result<Action
         .await
         .context(ApplyRoleRoleBindingSnafu)?;
 
+    let vector_aggregator_address = resolve_vector_aggregator_address(&opa, &client)
+        .await
+        .context(ResolveVectorAggregatorAddressSnafu)?;
+
     for (rolegroup_name, rolegroup_config) in role_server_config.iter() {
         let rolegroup = RoleGroupRef {
             cluster: opa_ref.clone(),
@@ -201,8 +228,13 @@ pub async fn reconcile_opa(opa: Arc<OpaCluster>, ctx: Arc<Ctx>) -> Result<Action
             .merged_config(&OpaRole::Server, &rolegroup)
             .context(FailedToResolveConfigSnafu)?;
 
-        let rg_configmap =
-            build_server_rolegroup_config_map(&opa, &resolved_product_image, &rolegroup)?;
+        let rg_configmap = build_server_rolegroup_config_map(
+            &opa,
+            &resolved_product_image,
+            &rolegroup,
+            &merged_config,
+            vector_aggregator_address.as_deref(),
+        )?;
         let rg_daemonset = build_server_rolegroup_daemonset(
             &opa,
             &resolved_product_image,
@@ -382,8 +414,12 @@ fn build_server_rolegroup_config_map(
     opa: &OpaCluster,
     resolved_product_image: &ResolvedProductImage,
     rolegroup: &RoleGroupRef<OpaCluster>,
+    merged_config: &OpaConfig,
+    vector_aggregator_address: Option<&str>,
 ) -> Result<ConfigMap> {
-    ConfigMapBuilder::new()
+    let mut cm_builder = ConfigMapBuilder::new();
+
+    cm_builder
         .metadata(
             ObjectMetaBuilder::new()
                 .name_and_namespace(opa)
@@ -398,7 +434,19 @@ fn build_server_rolegroup_config_map(
                 ))
                 .build(),
         )
-        .add_data(CONFIG_FILE, build_config_file())
+        .add_data(CONFIG_FILE, build_config_file());
+
+    extend_role_group_config_map(
+        rolegroup,
+        vector_aggregator_address,
+        &merged_config.logging,
+        &mut cm_builder,
+    )
+    .context(InvalidLoggingConfigSnafu {
+        cm_name: rolegroup.object_name(),
+    })?;
+
+    cm_builder
         .build()
         .with_context(|_| BuildRoleGroupConfigSnafu {
             rolegroup: rolegroup.clone(),
@@ -435,13 +483,90 @@ fn build_server_rolegroup_daemonset(
             ..EnvVar::default()
         })
         .collect::<Vec<_>>();
-    let container_opa = ContainerBuilder::new("opa")
-        .expect("invalid hard-coded container name")
+
+    let prepare_container_name = &Container::Prepare.to_string();
+    let mut prepare_container_args = vec![];
+    let mut cb_prepare = ContainerBuilder::new(&prepare_container_name).with_context(|_| {
+        IllegalContainerNameSnafu {
+            container_name: prepare_container_name,
+        }
+    })?;
+
+    if let Some(ContainerLogConfig {
+        choice: Some(ContainerLogConfigChoice::Automatic(log_config)),
+    }) = merged_config.logging.containers.get(&Container::Prepare)
+    {
+        prepare_container_args.push(product_logging::framework::capture_shell_output(
+            STACKABLE_LOG_DIR,
+            &prepare_container_name,
+            log_config,
+        ));
+    }
+
+    prepare_container_args.push(format!("mkdir -p {BUNDLES_ACTIVE_DIR}"));
+    prepare_container_args.push(format!("mkdir -p {BUNDLES_INCOMING_DIR}"));
+    prepare_container_args.push(format!("mkdir -p {BUNDLES_TMP_DIR}"));
+
+    let bundle_builder_container_name = &Container::BundleBuilder.to_string();
+    let mut cb_bundle_builder = ContainerBuilder::new(&bundle_builder_container_name)
+        .with_context(|_| IllegalContainerNameSnafu {
+            container_name: bundle_builder_container_name,
+        })?;
+
+    let opa_container_name = &Container::Opa.to_string();
+    let mut cb_opa =
+        ContainerBuilder::new(&opa_container_name).with_context(|_| IllegalContainerNameSnafu {
+            container_name: opa_container_name,
+        })?;
+
+    cb_prepare
+        .image_from_product_image(resolved_product_image)
+        .command(vec![
+            "bash".to_string(),
+            "-euo".to_string(),
+            "pipefail".to_string(),
+            "-x".to_string(),
+            "-c".to_string(),
+        ])
+        .args(vec![prepare_container_args.join(" && ")])
+        .add_volume_mount("bundles", "/bundles")
+        .add_volume_mount("log", STACKABLE_LOG_DIR);
+
+    cb_bundle_builder
+        .image_from_product_image(resolved_product_image)
+        .command(vec![String::from("/stackable/opa-bundle-builder")])
+        .add_env_var_from_field_path("WATCH_NAMESPACE", FieldPathEnvVar::Namespace)
+        .add_volume_mount("bundles", "/bundles")
+        .add_volume_mount("log", STACKABLE_LOG_DIR)
+        .readiness_probe(Probe {
+            initial_delay_seconds: Some(5),
+            period_seconds: Some(10),
+            failure_threshold: Some(5),
+            http_get: Some(HTTPGetAction {
+                port: IntOrString::Int(BUNDLE_BUILDER_PORT),
+                path: Some("/status".to_string()),
+                ..HTTPGetAction::default()
+            }),
+            ..Probe::default()
+        })
+        .liveness_probe(Probe {
+            initial_delay_seconds: Some(30),
+            period_seconds: Some(10),
+            http_get: Some(HTTPGetAction {
+                port: IntOrString::Int(BUNDLE_BUILDER_PORT),
+                path: Some("/status".to_string()),
+                ..HTTPGetAction::default()
+            }),
+            ..Probe::default()
+        });
+
+    cb_opa
         .image_from_product_image(resolved_product_image)
         .command(build_opa_start_command())
         .add_env_vars(env)
         .add_container_port(APP_PORT_NAME, APP_PORT.into())
         .add_volume_mount("config", "/stackable/config")
+        .add_volume_mount("log", STACKABLE_LOG_DIR)
         .resources(merged_config.resources.to_owned().into())
         .readiness_probe(Probe {
             initial_delay_seconds: Some(5),
@@ -461,56 +586,60 @@ fn build_server_rolegroup_daemonset(
                 ..HTTPGetAction::default()
             }),
             ..Probe::default()
-        })
-        .build();
+        });
 
-    let container_bundle_builder = ContainerBuilder::new("opa-bundle-builder")
-        .expect("invalid hard-coded container name")
-        .image_from_product_image(resolved_product_image)
-        .command(vec![String::from("/stackable/opa-bundle-builder")])
-        .add_env_var_from_field_path("WATCH_NAMESPACE", FieldPathEnvVar::Namespace)
-        .add_volume_mount("bundles", "/bundles")
-        .readiness_probe(Probe {
-            initial_delay_seconds: Some(5),
-            period_seconds: Some(10),
-            failure_threshold: Some(5),
-            http_get: Some(HTTPGetAction {
-                port: IntOrString::Int(BUNDLE_BUILDER_PORT),
-                path: Some("/status".to_string()),
-                ..HTTPGetAction::default()
-            }),
-            ..Probe::default()
-        })
-        .liveness_probe(Probe {
-            initial_delay_seconds: Some(30),
-            period_seconds: Some(10),
-            http_get: Some(HTTPGetAction {
-                port: IntOrString::Int(BUNDLE_BUILDER_PORT),
-                path: Some("/status".to_string()),
-                ..HTTPGetAction::default()
-            }),
-            ..Probe::default()
-        })
-        .build();
+    let mut pb = PodBuilder::new();
 
-    let init_container = ContainerBuilder::new("init-container")
-        .expect("invalid hard-coded container name")
-        .image_from_product_image(resolved_product_image)
-        .command(vec!["bash".to_string()])
-        .args(vec![
-            "-euo".to_string(),
-            "pipefail".to_string(),
-            "-x".to_string(),
-            "-c".to_string(),
-            [
-                format!("mkdir -p {BUNDLES_ACTIVE_DIR}"),
-                format!("mkdir -p {BUNDLES_INCOMING_DIR}"),
-                format!("mkdir -p {BUNDLES_TMP_DIR}"),
-            ]
-            .join(" && "),
-        ])
-        .add_volume_mount("bundles", "/bundles")
-        .build();
+    pb.metadata_builder(|m| {
+        m.with_recommended_labels(build_recommended_labels(
+            opa,
+            &resolved_product_image.app_version_label,
+            &rolegroup_ref.role,
+            &rolegroup_ref.role_group,
+        ))
+    })
+    .add_init_container(cb_prepare.build())
+    .add_container(cb_opa.build())
+    .add_container(cb_bundle_builder.build())
+    .image_pull_secrets_from_product_image(resolved_product_image)
+    .add_volume(Volume {
+        name: "config".to_string(),
+        config_map: Some(ConfigMapVolumeSource {
+            name: Some(rolegroup_ref.object_name()),
+            ..ConfigMapVolumeSource::default()
+        }),
+        ..Volume::default()
+    })
+    .add_volume(Volume {
+        name: "bundles".to_string(),
+        empty_dir: Some(EmptyDirVolumeSource::default()),
+        ..Volume::default()
+    })
+    .add_volume(Volume {
+        name: "log".to_string(),
+        empty_dir: Some(EmptyDirVolumeSource {
+            medium: None,
+            // TODO: determine size
+            size_limit: Some(Quantity(format!("20Mi"))),
+        }),
+        ..Volume::default()
+    })
+    .service_account_name(sa_name)
+    .security_context(PodSecurityContext {
+        run_as_user: Some(1000),
+        run_as_group: Some(1000),
+        fs_group: Some(1000),
+        ..PodSecurityContext::default()
+    });
+
+    if merged_config.logging.enable_vector_agent {
+        pb.add_container(product_logging::framework::vector_container(
+            resolved_product_image,
+            "config",
+            "log",
+            merged_config.logging.containers.get(&Container::Vector),
+        ));
+    }
 
     Ok(DaemonSet {
         metadata: ObjectMetaBuilder::new()
@@ -535,40 +664,7 @@ fn build_server_rolegroup_daemonset(
                 )),
                 ..LabelSelector::default()
             },
-            template: PodBuilder::new()
-                .metadata_builder(|m| {
-                    m.with_recommended_labels(build_recommended_labels(
-                        opa,
-                        &resolved_product_image.app_version_label,
-                        &rolegroup_ref.role,
-                        &rolegroup_ref.role_group,
-                    ))
-                })
-                .add_init_container(init_container)
-                .add_container(container_opa)
-                .add_container(container_bundle_builder)
-                .image_pull_secrets_from_product_image(resolved_product_image)
-                .add_volume(Volume {
-                    name: "config".to_string(),
-                    config_map: Some(ConfigMapVolumeSource {
-                        name: Some(rolegroup_ref.object_name()),
-                        ..ConfigMapVolumeSource::default()
-                    }),
-                    ..Volume::default()
-                })
-                .add_volume(Volume {
-                    name: "bundles".to_string(),
-                    empty_dir: Some(EmptyDirVolumeSource::default()),
-                    ..Volume::default()
-                })
-                .service_account_name(sa_name)
-                .security_context(PodSecurityContext {
-                    run_as_user: Some(1000),
-                    run_as_group: Some(1000),
-                    fs_group: Some(1000),
-                    ..PodSecurityContext::default()
-                })
-                .build_template(),
+            template: pb.build_template(),
             ..DaemonSetSpec::default()
         }),
         status: None,
@@ -592,7 +688,11 @@ bundles:
     persist: true
     polling:
       min_delay_seconds: 10
-      max_delay_seconds: 20"
+      max_delay_seconds: 20
+
+decision_logs:
+    console: true
+"
 }
 
 fn build_opa_start_command() -> Vec<String> {
@@ -604,6 +704,11 @@ fn build_opa_start_command() -> Vec<String> {
         format!("0.0.0.0:{}", APP_PORT),
         "-c".to_string(),
         "/stackable/config/config.yaml".to_string(),
+        // TODO: test
+        "-l".to_string(),
+        "info".to_string(),
+        "--log-format".to_string(),
+        "json".to_string(),
     ]
 }
 
