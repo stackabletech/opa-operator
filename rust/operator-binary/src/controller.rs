2148fa1d57bd62f284642b3e0048369216571654
Sebastian Bernauer <sebastian.bernauer@stackable.de>
2023-04-06 12:41:26 +0200

    Support specifying Service type

--- rust/operator-binary/src/controller.rs
+++ rust/operator-binary/src/controller.rs
@@ -399,6 +399,7 @@ pub fn build_server_role_service(
             ))
             .build(),
         spec: Some(ServiceSpec {
+            type_: Some(opa.spec.cluster_config.listener_class.k8s_service_type()),
             ports: Some(vec![ServicePort {
                 name: Some(APP_PORT_NAME.to_string()),
                 port: APP_PORT.into(),
@@ -406,7 +407,6 @@ pub fn build_server_role_service(
                 ..ServicePort::default()
             }]),
             selector: Some(role_selector_labels(opa, APP_NAME, &role_name)),
-            type_: Some("NodePort".to_string()),
             internal_traffic_policy: Some("Local".to_string()),
             ..ServiceSpec::default()
         }),
@@ -437,6 +437,8 @@ fn build_rolegroup_service(
             .with_label("prometheus.io/scrape", "true")
             .build(),
         spec: Some(ServiceSpec {
+            // Internal communication does not need to be exposed
+            type_: Some("ClusterIP".to_string()),
             cluster_ip: Some("None".to_string()),
             ports: Some(service_ports()),
             selector: Some(role_group_selector_labels(
