3092b7555da7cf6f46230433d6e2a85f987abeb7
Nick Larsen <nick.larsen@stackable.tech>
2023-11-30 14:18:53 +0100

    tests probably pass now

--- tests/templates/kuttl/keycloak-user-info/test-regorule.py
+++ tests/templates/kuttl/keycloak-user-info/test-regorule.py
@@ -3,37 +3,86 @@ import requests
 import argparse
 import json
 
-# test table for users
-# alice and bob by name first, then by id
+# Example result payloads
+# {
+#   "result": {
+#     "currentUserInfoByUsername": {
+#       "customAttributes": {},
+#       "groups": [
+#         "/superset-admin"
+#       ],
+#       "id": "af07f12c-a2db-40a7-93e0-874537bdf3f5",
+#       "username": "alice"
+#     }
+#   }
+# }
 
+# {
+#   "result": {
+#     "currentUserInfoById": {
+#       "customAttributes": {},
+#       "groups": [
+#         "/superset-admin"
+#       ],
+#       "id": "af07f12c-a2db-40a7-93e0-874537bdf3f5",
+#       "username": "alice"
+#     }
+#   }
+# }
+
+# todo: make the test more comprehensive to check customAttributes
+users_and_groups = {
+    "alice": ["/superset-admin"],
+    "bob": [],
+}
+
+
+def assertions(username, response, opa_attribute, expected_groups, expected_attributes = {}):
+    assert "result" in response
+    assert opa_attribute in response["result"]
+
+    # repeated the right hand side for better output on error
+    assert "customAttributes" in response["result"][opa_attribute]
+    assert "groups" in response["result"][opa_attribute]
+    assert "id" in response["result"][opa_attribute]
+    assert "username" in response["result"][opa_attribute]
+
+    # todo: split out group assertions
+    print(f"Testing for {username} in groups {expected_groups}")
+    groups = sorted(response["result"][opa_attribute]["groups"])
+    expected_groups = sorted(expected_groups)
+    assert groups == expected_groups, f"got {groups}, expected: {expected_groups}"
+   
+    # todo: split out customAttribute assertions
+    print(f"Testing for {username} with customAttributes {expected_attributes}")
+    custom_attributes = response["result"][opa_attribute]["customAttributes"]
+    assert custom_attributes == expected_attributes, f"got {custom_attributes}, expected: {expected_attributes}"
 
 if __name__ == "__main__":
     all_args = argparse.ArgumentParser()
     all_args.add_argument("-u", "--url", required=True, help="OPA service url")
     args = vars(all_args.parse_args())
-
-    # todo: we removed currentUserInfo frin the path
-    # todo: try this out locally until it works
-    # url = 'http://test-opa-svc:8081/v1/data/currentUserInfo'
     params = {'strict-builtin-errors': 'true'}
-    payload = {'input': {'username': 'alice'}}
-    response = requests.post(args['url'], data=json.dumps(payload), params=params).json()
 
-    expected_response = {
-        'customAttributes': {},
-        'groups': ["/superset-admin"],
-    }
+    def make_request(payload):
+        return requests.post(args['url'], data=json.dumps(payload), params=params).json()
 
-    # Sort lists since their order is not well-defined..
-    response.get("result", {}).get("groups", []).sort(key=lambda group: group.get("name", ""))
+    for username, groups in users_and_groups.items():
+        try:
+            # todo: try this out locally until it works
+            # url = 'http://test-opa-svc:8081/v1/data'
+            payload = {'input': {'username': username}}
+            response = make_request(payload)
+            assertions(username, response, "currentUserInfoByUsername", groups, {})
+            
+            # do the reverse lookup
+            user_id = response["result"]["currentUserInfoByUsername"]["id"]
+            payload = {'input': {'id': user_id}}
+            response = make_request(payload)
+            assertions(username, response, "currentUserInfoById", groups, {})
+        except Exception as e:
+            if response is not None:
+                print(f"something went wrong. last response: {response}")
+            raise e
 
-    if "result" in response and response["result"] == expected_response:
-        print("Test successful!")
-        exit(0)
-    else:
-        print(f"Error: received {response} - expected: {({'result': expected_response})}")
-        exit(-1)
-
-
-received: {'result': {'currentUserInfoByUsername': {'customAttributes': {}, 'groups': ['/superset-admin'], 'id': '1680586a-f61d-4624-acf0-d32f3adb4427', 'username': 'alice'}}}
-expected: {'result': {'customAttributes': {}, 'groups': ['/g1']}}
+    print("Test successful!")
