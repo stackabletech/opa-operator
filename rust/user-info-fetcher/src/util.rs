89aac83a42cec311940e7b67b7bf11cdeaab4afb
Felix Hennig <felix.hennig@stackable.tech>
2024-01-31 14:20:20 +0100

    WIP

--- rust/user-info-fetcher/src/util.rs
+++ rust/user-info-fetcher/src/util.rs
@@ -1,8 +1,28 @@
 use reqwest::RequestBuilder;
 use serde::de::DeserializeOwned;
+use snafu::Snafu;
+
+// TODO write an error type in here
+
+#[derive(Snafu, Debug)]
+pub enum Error {
+    #[snafu(display("failed to execute request"))]
+    HttpRequest { source: reqwest::Error },
+
+    #[snafu(display("failed to parse json response"))]
+    ParseJson { source: reqwest::Error },
+
+    #[snafu(display("response was an http error"))]
+    HttpErrorResponse { source: reqwest::Error },
+
+    #[snafu(display("response was an http error: {text}"))]
+    Something { text: String },
+}
 
 pub async fn send_json_request<T: DeserializeOwned>(
     req: RequestBuilder,
 ) -> Result<T, reqwest::Error> {
-    req.send().await?.error_for_status()?.json().await
+    // TODO check for differen error sources and send informative errors from here
+    let response =  req.send().await?;
+    response.error_for_status()?.json().await
 }
