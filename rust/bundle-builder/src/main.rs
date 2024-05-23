881950f9c7f2cfcf90ee728d1701ff671277feb9
Natalie Klestrup Röijezon <nat@nullable.se>
2024-05-23 15:30:45 +0200

    Handle and report errors

--- rust/bundle-builder/src/main.rs
+++ rust/bundle-builder/src/main.rs
@@ -1,6 +1,5 @@
 use std::{
-    net::AddrParseError,
-    path::PathBuf,
+    num::TryFromIntError,
     sync::{Arc, Mutex},
 };
 
@@ -9,13 +8,14 @@ use clap::Parser;
 use flate2::write::GzEncoder;
 use futures::{
     future::{self, BoxFuture},
-    pin_mut, FutureExt, StreamExt,
+    pin_mut, FutureExt, StreamExt, TryFutureExt,
 };
 use snafu::{ResultExt, Snafu};
 use stackable_operator::{
     k8s_openapi::api::core::v1::ConfigMap,
     kube::{
         self,
+        api::ObjectMeta,
         runtime::{
             reflector::{self, ObjectRef, Store},
             watcher,
@@ -34,8 +34,8 @@ pub struct Args {
     common: stackable_operator::cli::ProductOperatorRun,
 }
 
-type Bundle = Arc<[u8]>;
-type BundleFuture = future::Shared<BoxFuture<'static, Bundle>>;
+type Bundle = Vec<u8>;
+type BundleFuture = future::Shared<BoxFuture<'static, Arc<Result<Bundle, BundleError>>>>;
 
 #[derive(Clone)]
 struct AppState {
@@ -44,11 +44,8 @@ struct AppState {
 
 #[derive(Snafu, Debug)]
 enum StartupError {
-    #[snafu(display("unable to read config file from {path:?}"))]
-    ReadConfigFile {
-        source: std::io::Error,
-        path: PathBuf,
-    },
+    #[snafu(display("failed to initialize Kubernetes client"))]
+    InitKube { source: kube::Error },
     #[snafu(display("failed to get listener address"))]
     GetListenerAddr { source: std::io::Error },
     #[snafu(display("failed to register SIGTERM handler"))]
@@ -69,10 +66,21 @@ async fn main() -> Result<(), StartupError> {
         args.common.tracing_target,
     );
 
-    let kube = kube::Client::try_default().await.unwrap();
+    let kube = kube::Client::try_default().await.context(InitKubeSnafu)?;
 
     let (store, store_w) = reflector::store();
-    let rebuild_bundle = || build_bundle(store.clone()).map(Arc::from).boxed().shared();
+    let rebuild_bundle = || {
+        build_bundle(store.clone())
+            .inspect_err(|error| {
+                error!(
+                    error = error as &dyn std::error::Error,
+                    "failed to rebuild bundle"
+                )
+            })
+            .map(Arc::from)
+            .boxed()
+            .shared()
+    };
     let bundle = Arc::new(Mutex::new(rebuild_bundle()));
     let reflector = std::pin::pin!(reflector::reflector(
         store_w,
@@ -145,48 +153,116 @@ async fn main() -> Result<(), StartupError> {
     future::select(reflector, server).await.factor_first().0
 }
 
-async fn build_bundle(store: Store<ConfigMap>) -> Vec<u8> {
-    fn file_header(data: &[u8]) -> tar::Header {
+#[derive(Snafu, Debug)]
+#[snafu(module)]
+enum BundleError {
+    #[snafu(display("ConfigMap is missing required metadata"))]
+    ConfigMapMetadataMissing,
+
+    #[snafu(display("file {file_name:?} in {config_map} is too large ({file_size} bytes)"))]
+    FileSizeOverflow {
+        source: TryFromIntError,
+        config_map: ObjectRef<ConfigMap>,
+        file_name: String,
+        file_size: usize,
+    },
+
+    #[snafu(display("failed to add file {file_name:?} from {config_map} to tarball"))]
+    AddFileToTarball {
+        source: std::io::Error,
+        config_map: ObjectRef<ConfigMap>,
+        file_name: String,
+    },
+
+    #[snafu(display("failed to build tarball"))]
+    BuildTarball { source: std::io::Error },
+}
+
+impl BundleError {
+    fn to_http_response(&self) -> impl IntoResponse {
+        (
+            http::StatusCode::INTERNAL_SERVER_ERROR,
+            "failed to build bundle, see opa-bundle-builder logs for more details",
+        )
+    }
+}
+
+async fn build_bundle(store: Store<ConfigMap>) -> Result<Vec<u8>, BundleError> {
+    use bundle_error::*;
+    fn file_header(
+        config_map: &ObjectRef<ConfigMap>,
+        file_name: &str,
+        data: &[u8],
+    ) -> Result<tar::Header, BundleError> {
         let mut header = tar::Header::new_gnu();
         header.set_mode(0o644);
-        header.set_size(data.len().try_into().unwrap());
+        let file_size = data.len();
+        header.set_size(
+            file_size
+                .try_into()
+                .with_context(|_| FileSizeOverflowSnafu {
+                    config_map: config_map.clone(),
+                    file_name,
+                    file_size,
+                })?,
+        );
         header.set_entry_type(tar::EntryType::Regular);
-        header
+        header.set_cksum();
+        Ok(header)
     }
 
     info!("building bundle");
     let mut tar = tar::Builder::new(GzEncoder::new(Vec::new(), flate2::Compression::default()));
     for cm in store.state() {
-        let cm_ns = cm.metadata.namespace.as_deref().unwrap();
-        let cm_name = cm.metadata.name.as_deref().unwrap();
+        let ObjectMeta {
+            name: Some(cm_ns),
+            namespace: Some(cm_name),
+            ..
+        } = &cm.metadata
+        else {
+            return ConfigMapMetadataMissingSnafu.fail();
+        };
+        let cm_ref = ObjectRef::from_obj(&*cm);
         for (file_name, data) in cm.data.iter().flatten() {
-            let mut header = file_header(data.as_bytes());
+            let mut header = file_header(&cm_ref, file_name, data.as_bytes())?;
             tar.append_data(
                 &mut header,
                 format!("configmap/{cm_ns}/{cm_name}/{file_name}"),
                 data.as_bytes(),
             )
-            .unwrap();
+            .with_context(|_| AddFileToTarballSnafu {
+                config_map: cm_ref.clone(),
+                file_name,
+            })?;
         }
     }
-    let tar = tar.into_inner().unwrap().finish().unwrap();
+    let tar = tar
+        .into_inner()
+        .context(BuildTarballSnafu)?
+        .finish()
+        .context(BuildTarballSnafu)?;
     info!("finished building bundle");
-    tar
+    Ok(tar)
 }
 
 async fn get_status(State(state): State<AppState>) -> impl IntoResponse {
     let bundle = future::Shared::clone(&*state.bundle.lock().unwrap());
-    bundle.await;
-    "ready"
+    if let Err(err) = bundle.await.as_deref() {
+        return Err(err.to_http_response());
+    }
+    Ok("ready")
 }
 
 async fn get_bundle(State(state): State<AppState>) -> impl IntoResponse {
     let bundle = future::Shared::clone(&*state.bundle.lock().unwrap());
-    (
+    Ok((
         [(
             http::header::CONTENT_TYPE,
             http::HeaderValue::from_static("application/gzip"),
         )],
-        bundle.await.to_vec(),
-    )
+        match bundle.await.as_deref() {
+            Ok(bundle) => bundle.to_vec(),
+            Err(err) => return Err(err.to_http_response()),
+        },
+    ))
 }
