e0fecaa9d75d1454c4e1862d9f46c8b704a83021
Nick Larsen <nick.larsen@stackable.tech>
2024-08-09 10:15:45 +0200

    chore(kuttl): ruff formatting

--- tests/templates/kuttl/aas-user-info/test-regorule.py
+++ tests/templates/kuttl/aas-user-info/test-regorule.py
@@ -4,7 +4,9 @@ import argparse
 import json
 
 
-def assertions(username, response, opa_attribute, expected_groups, expected_attributes={}):
+def assertions(
+    username, response, opa_attribute, expected_groups, expected_attributes={}
+):
     assert "result" in response
     assert opa_attribute in response["result"]
 
@@ -23,25 +25,35 @@ def assertions(username, response, opa_attribute, expected_groups, expected_attr
     # todo: split out customAttribute assertions
     print(f"Testing for {username} with customAttributes {expected_attributes}")
     custom_attributes = response["result"][opa_attribute]["customAttributes"]
-    assert custom_attributes == expected_attributes, f"got {custom_attributes}, expected: {expected_attributes}"
+    assert (
+        custom_attributes == expected_attributes
+    ), f"got {custom_attributes}, expected: {expected_attributes}"
 
 
 if __name__ == "__main__":
     all_args = argparse.ArgumentParser()
     all_args.add_argument("-u", "--url", required=True, help="OPA service url")
     args = vars(all_args.parse_args())
-    params = {'strict-builtin-errors': 'true'}
+    params = {"strict-builtin-errors": "true"}
 
     def make_request(payload):
-        return requests.post(args['url'], data=json.dumps(payload), params=params).json()
+        return requests.post(
+            args["url"], data=json.dumps(payload), params=params
+        ).json()
 
     for subject_id in ["alice", "bob"]:
         try:
             # todo: try this out locally until it works
             # url = 'http://test-opa-svc:8081/v1/data'
-            payload = {'input': {'id': subject_id}}
+            payload = {"input": {"id": subject_id}}
             response = make_request(payload)
-            assertions(subject_id, response, "currentUserInfoById", [], {"e-mail": f"{subject_id}@example.com", "company": "openid"})
+            assertions(
+                subject_id,
+                response,
+                "currentUserInfoById",
+                [],
+                {"e-mail": f"{subject_id}@example.com", "company": "openid"},
+            )
         except Exception as e:
             print(f"exception: {e}")
             if response is not None:
--- tests/templates/kuttl/keycloak-user-info/test-regorule.py
+++ tests/templates/kuttl/keycloak-user-info/test-regorule.py
@@ -10,7 +10,9 @@ users_and_groups = {
 }
 
 
-def assertions(username, response, opa_attribute, expected_groups, expected_attributes={}):
+def assertions(
+    username, response, opa_attribute, expected_groups, expected_attributes={}
+):
     assert "result" in response
     assert opa_attribute in response["result"]
 
@@ -29,29 +31,33 @@ def assertions(username, response, opa_attribute, expected_groups, expected_attr
     # todo: split out customAttribute assertions
     print(f"Testing for {username} with customAttributes {expected_attributes}")
     custom_attributes = response["result"][opa_attribute]["customAttributes"]
-    assert custom_attributes == expected_attributes, f"got {custom_attributes}, expected: {expected_attributes}"
+    assert (
+        custom_attributes == expected_attributes
+    ), f"got {custom_attributes}, expected: {expected_attributes}"
 
 
 if __name__ == "__main__":
     all_args = argparse.ArgumentParser()
     all_args.add_argument("-u", "--url", required=True, help="OPA service url")
     args = vars(all_args.parse_args())
-    params = {'strict-builtin-errors': 'true'}
+    params = {"strict-builtin-errors": "true"}
 
     def make_request(payload):
-        return requests.post(args['url'], data=json.dumps(payload), params=params).json()
+        return requests.post(
+            args["url"], data=json.dumps(payload), params=params
+        ).json()
 
     for username, groups in users_and_groups.items():
         try:
             # todo: try this out locally until it works
             # url = 'http://test-opa-svc:8081/v1/data'
-            payload = {'input': {'username': username}}
+            payload = {"input": {"username": username}}
             response = make_request(payload)
             assertions(username, response, "currentUserInfoByUsername", groups, {})
 
             # do the reverse lookup
             user_id = response["result"]["currentUserInfoByUsername"]["id"]
-            payload = {'input': {'id': user_id}}
+            payload = {"input": {"id": user_id}}
             response = make_request(payload)
             assertions(username, response, "currentUserInfoById", groups, {})
         except Exception as e:
