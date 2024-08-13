1477028598eae11daf95ca27bc23c2c311e7e071
Nick Larsen <nick.larsen@stackable.tech>
2024-08-13 13:00:39 +0200

    chore(kuttl): improve error output in python scripts

--- tests/templates/kuttl/aas-user-info/test-regorule.py
+++ tests/templates/kuttl/aas-user-info/test-regorule.py
@@ -8,23 +8,24 @@ def assertions(
     username, response, opa_attribute, expected_groups, expected_attributes={}
 ):
     assert "result" in response
-    assert opa_attribute in response["result"]
+    result = response["result"]
+    assert opa_attribute in result, f"expected {opa_attribute} in {result}"
 
     # repeated the right hand side for better output on error
-    assert "customAttributes" in response["result"][opa_attribute]
-    assert "groups" in response["result"][opa_attribute]
-    assert "id" in response["result"][opa_attribute]
-    assert "username" in response["result"][opa_attribute]
+    assert "customAttributes" in result[opa_attribute]
+    assert "groups" in result[opa_attribute]
+    assert "id" in result[opa_attribute]
+    assert "username" in result[opa_attribute]
 
     # todo: split out group assertions
     print(f"Testing for {username} in groups {expected_groups}")
-    groups = sorted(response["result"][opa_attribute]["groups"])
+    groups = sorted(result[opa_attribute]["groups"])
     expected_groups = sorted(expected_groups)
     assert groups == expected_groups, f"got {groups}, expected: {expected_groups}"
 
     # todo: split out customAttribute assertions
     print(f"Testing for {username} with customAttributes {expected_attributes}")
-    custom_attributes = response["result"][opa_attribute]["customAttributes"]
+    custom_attributes = result[opa_attribute]["customAttributes"]
     assert (
         custom_attributes == expected_attributes
     ), f"got {custom_attributes}, expected: {expected_attributes}"
@@ -37,9 +38,12 @@ if __name__ == "__main__":
     params = {"strict-builtin-errors": "true"}
 
     def make_request(payload):
-        return requests.post(
-            args["url"], data=json.dumps(payload), params=params
-        ).json()
+        response = requests.post(args["url"], data=json.dumps(payload), params=params)
+        expected_status_code = 200
+        assert (
+            response.status_code == expected_status_code
+        ), f"got {response.status_code}, expected: {expected_status_code}"
+        return response.json()
 
     for subject_id in ["alice", "bob"]:
         try:
