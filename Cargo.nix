a16edea53d5c25789970861dfa32b497e68592bc
Nick <10092581+NickLarsenNZ@users.noreply.github.com>
2025-03-24 14:13:06 +0100

    chore: Release 25.3.0 (#700)
    Signed-off-by: Nick Larsen <nick.larsen@stackable.tech>

--- Cargo.nix
+++ Cargo.nix
@@ -9315,7 +9315,7 @@ rec {
       };
       "stackable-opa-bundle-builder" = rec {
         crateName = "stackable-opa-bundle-builder";
-        version = "0.0.0-dev";
+        version = "25.3.0";
         edition = "2021";
         crateBin = [
           {
@@ -9379,7 +9379,7 @@ rec {
       };
       "stackable-opa-operator" = rec {
         crateName = "stackable-opa-operator";
-        version = "0.0.0-dev";
+        version = "25.3.0";
         edition = "2021";
         crateBin = [
           {
@@ -9478,7 +9478,7 @@ rec {
       };
       "stackable-opa-regorule-library" = rec {
         crateName = "stackable-opa-regorule-library";
-        version = "0.0.0-dev";
+        version = "25.3.0";
         edition = "2021";
         src = lib.cleanSourceWith { filter = sourceFilter;  src = ./rust/regorule-library; };
         libName = "stackable_opa_regorule_library";
@@ -9489,7 +9489,7 @@ rec {
       };
       "stackable-opa-user-info-fetcher" = rec {
         crateName = "stackable-opa-user-info-fetcher";
-        version = "0.0.0-dev";
+        version = "25.3.0";
         edition = "2021";
         crateBin = [
           {
