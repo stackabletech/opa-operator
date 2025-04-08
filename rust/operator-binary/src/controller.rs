2d1f5adce84caa39ad900b01550c86213d8615f4
Nick Larsen <nick.larsen@stackable.tech>
2025-04-08 10:04:50 +0200

    fix: Set correct env var (ROLLING_LOGS_DIR) for bundle-builder pod

--- rust/operator-binary/src/controller.rs
+++ rust/operator-binary/src/controller.rs
@@ -816,7 +816,7 @@ fn build_server_rolegroup_daemonset(
             bundle_builder_log_level(merged_config).to_string(),
         )
         .add_env_var(
-            "OPA_BUNDLE_BUILDER_LOG_DIRECTORY",
+            "ROLLING_LOGS_DIR",
             format!("{STACKABLE_LOG_DIR}/{bundle_builder_container_name}"),
         )
         .add_volume_mount(BUNDLES_VOLUME_NAME, BUNDLES_DIR)
