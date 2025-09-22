ec2fc7acc1e5a13124601fde8040e5431a80fbad
Xenia <xenia.fischer@stackable.tech>
2025-09-22 17:05:58 +0200

    chore: Bump vector version for 25.11.0 (#763)
    * chore: Bump vector version for 25.11.0
    
    * update service name in test

--- tests/templates/kuttl/logging/test_log_aggregation.sh
+++ tests/templates/kuttl/logging/test_log_aggregation.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-DECISION_LOGS=$(kubectl logs service/test-opa -c opa | grep "decision_id");
+DECISION_LOGS=$(kubectl logs service/test-opa-server -c opa | grep "decision_id");
 
 if [ -n "$DECISION_LOGS" ];
 then
