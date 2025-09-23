201537da62bb65a92b889b76a66655618884cd33
Andrew Kenworthy <1712947+adwk67@users.noreply.github.com>
2025-09-23 09:24:33 +0200

    do not mask kubectl errors but do not crash on grep (#764)

--- tests/templates/kuttl/logging/test_log_aggregation.sh
+++ tests/templates/kuttl/logging/test_log_aggregation.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-DECISION_LOGS=$(kubectl logs service/test-opa-server -c opa | grep "decision_id");
+# we should distinguish between cases where the variable is not set
+# if the service or container is not found (which we don't want), and where
+# grep does not find anything (which is what we *do* want).
+
+set -Eeuo pipefail
+
+# do not mask kubectl errors, but the right-hand group must always be true
+# so the test does not fail on the grep
+DECISION_LOGS=$(kubectl logs service/test-opa-server -c opa | { grep "decision_id" || true; })
 
 if [ -n "$DECISION_LOGS" ];
 then
