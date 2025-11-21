5d7f25a72fb24dc1b3652b57b543d96d7f58f930
dervoeti <lukas.krug@stackable.tech>
2025-11-21 16:47:15 +0100

    refactor: move initialization into backends

--- rust/user-info-fetcher/src/backend/keycloak.rs
+++ rust/user-info-fetcher/src/backend/keycloak.rs
@@ -1,12 +1,16 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, path::Path};
 
 use hyper::StatusCode;
+use reqwest::ClientBuilder;
 use serde::Deserialize;
 use snafu::{OptionExt, ResultExt, Snafu};
 use stackable_opa_operator::crd::user_info_fetcher::v1alpha1;
 use stackable_operator::crd::authentication::oidc;
 
-use crate::{Credentials, UserInfo, UserInfoRequest, http_error, utils::http::send_json_request};
+use crate::{
+    UserInfo, UserInfoRequest, http_error,
+    utils::{self, http::send_json_request},
+};
 
 #[derive(Snafu, Debug)]
 pub enum Error {
@@ -42,6 +46,24 @@ pub enum Error {
 
     #[snafu(display("failed to construct OIDC endpoint path"))]
     ConstructOidcEndpointPath { source: url::ParseError },
+
+    #[snafu(display("failed to construct HTTP client"))]
+    ConstructHttpClient { source: reqwest::Error },
+
+    #[snafu(display("failed to configure TLS"))]
+    ConfigureTls { source: utils::tls::Error },
+
+    #[snafu(display("failed to read client ID from {path:?}"))]
+    ReadClientId {
+        source: std::io::Error,
+        path: String,
+    },
+
+    #[snafu(display("failed to read client secret from {path:?}"))]
+    ReadClientSecret {
+        source: std::io::Error,
+        path: String,
+    },
 }
 
 impl http_error::Error for Error {
@@ -55,6 +77,10 @@ impl http_error::Error for Error {
             Self::RequestUserGroups { .. } => StatusCode::BAD_GATEWAY,
             Self::ParseOidcEndpointUrl { .. } => StatusCode::INTERNAL_SERVER_ERROR,
             Self::ConstructOidcEndpointPath { .. } => StatusCode::INTERNAL_SERVER_ERROR,
+            Self::ConstructHttpClient { .. } => StatusCode::SERVICE_UNAVAILABLE,
+            Self::ConfigureTls { .. } => StatusCode::SERVICE_UNAVAILABLE,
+            Self::ReadClientId { .. } => StatusCode::SERVICE_UNAVAILABLE,
+            Self::ReadClientSecret { .. } => StatusCode::SERVICE_UNAVAILABLE,
         }
     }
 }
@@ -86,110 +112,160 @@ struct GroupMembership {
     path: String,
 }
 
-pub(crate) async fn get_user_info(
-    req: &UserInfoRequest,
-    http: &reqwest::Client,
-    credentials: &Credentials,
-    config: &v1alpha1::KeycloakBackend,
-) -> Result<UserInfo, Error> {
-    let v1alpha1::KeycloakBackend {
-        client_credentials_secret: _,
-        admin_realm,
-        user_realm,
-        hostname,
-        port,
-        root_path,
-        tls,
-    } = config;
+/// Keycloak backend with resolved credentials.
+///
+/// This struct combines the CRD configuration with credentials loaded from the filesystem.
+/// Credentials and the HTTP client are initialized once at startup and stored internally.
+pub struct ResolvedKeycloakBackend {
+    config: v1alpha1::KeycloakBackend,
+    client_id: String,
+    client_secret: String,
+    http_client: reqwest::Client,
+}
 
-    // We re-use existent functionality from operator-rs, besides it being a bit of miss-use.
-    // Some attributes (such as principal_claim) are irrelevant, and will not be read by the code-flow we trigger.
-    let wrapping_auth_provider = oidc::v1alpha1::AuthenticationProvider::new(
-        hostname.clone(),
-        *port,
-        root_path.clone(),
-        tls.clone(),
-        String::new(),
-        Vec::new(),
-        None,
-    );
-    let keycloak_url = wrapping_auth_provider
-        .endpoint_url()
-        .context(ParseOidcEndpointUrlSnafu)?;
+impl ResolvedKeycloakBackend {
+    /// Resolves a Keycloak backend by loading credentials from the filesystem.
+    ///
+    /// Reads `clientId` and `clientSecret` from the credentials directory and initializes
+    /// the HTTP client with appropriate TLS configuration.
+    pub async fn resolve(
+        config: v1alpha1::KeycloakBackend,
+        credentials_dir: &Path,
+    ) -> Result<Self, Error> {
+        let client_id_path = credentials_dir.join("clientId");
+        let client_secret_path = credentials_dir.join("clientSecret");
 
-    let authn = send_json_request::<OAuthResponse>(
-        http.post(
-            keycloak_url
-                .join(&format!(
-                    "realms/{admin_realm}/protocol/openid-connect/token"
-                ))
-                .context(ConstructOidcEndpointPathSnafu)?,
+        let client_id =
+            tokio::fs::read_to_string(&client_id_path)
+                .await
+                .context(ReadClientIdSnafu {
+                    path: client_id_path.display().to_string(),
+                })?;
+        let client_secret = tokio::fs::read_to_string(&client_secret_path)
+            .await
+            .context(ReadClientSecretSnafu {
+                path: client_secret_path.display().to_string(),
+            })?;
+
+        let mut client_builder = ClientBuilder::new();
+        client_builder = utils::tls::configure_reqwest(&config.tls, client_builder)
+            .await
+            .context(ConfigureTlsSnafu)?;
+        let http_client = client_builder.build().context(ConstructHttpClientSnafu)?;
+
+        Ok(Self {
+            config,
+            client_id,
+            client_secret,
+            http_client,
+        })
+    }
+
+    pub(crate) async fn get_user_info(&self, req: &UserInfoRequest) -> Result<UserInfo, Error> {
+        let v1alpha1::KeycloakBackend {
+            client_credentials_secret: _,
+            admin_realm,
+            user_realm,
+            hostname,
+            port,
+            root_path,
+            tls,
+        } = &self.config;
+
+        // We re-use existent functionality from operator-rs, besides it being a bit of miss-use.
+        // Some attributes (such as principal_claim) are irrelevant, and will not be read by the code-flow we trigger.
+        let wrapping_auth_provider = oidc::v1alpha1::AuthenticationProvider::new(
+            hostname.clone(),
+            *port,
+            root_path.clone(),
+            tls.clone(),
+            String::new(),
+            Vec::new(),
+            None,
+        );
+        let keycloak_url = wrapping_auth_provider
+            .endpoint_url()
+            .context(ParseOidcEndpointUrlSnafu)?;
+
+        let authn = send_json_request::<OAuthResponse>(
+            self.http_client
+                .post(
+                    keycloak_url
+                        .join(&format!(
+                            "realms/{admin_realm}/protocol/openid-connect/token"
+                        ))
+                        .context(ConstructOidcEndpointPathSnafu)?,
+                )
+                .basic_auth(&self.client_id, Some(&self.client_secret))
+                .form(&[("grant_type", "client_credentials")]),
         )
-        .basic_auth(&credentials.client_id, Some(&credentials.client_secret))
-        .form(&[("grant_type", "client_credentials")]),
-    )
-    .await
-    .context(AccessTokenSnafu)?;
+        .await
+        .context(AccessTokenSnafu)?;
 
-    let users_base_url = keycloak_url
-        .join(&format!("admin/realms/{user_realm}/users/"))
-        .context(ConstructOidcEndpointPathSnafu)?;
+        let users_base_url = keycloak_url
+            .join(&format!("admin/realms/{user_realm}/users/"))
+            .context(ConstructOidcEndpointPathSnafu)?;
 
-    let user_info = match req {
-        UserInfoRequest::UserInfoRequestById(req) => {
-            let user_id = req.id.clone();
-            send_json_request::<UserMetadata>(
-                http.get(
+        let user_info = match req {
+            UserInfoRequest::UserInfoRequestById(req) => {
+                let user_id = req.id.clone();
+                send_json_request::<UserMetadata>(
+                    self.http_client
+                        .get(
+                            users_base_url
+                                .join(&req.id)
+                                .context(ConstructOidcEndpointPathSnafu)?,
+                        )
+                        .bearer_auth(&authn.access_token),
+                )
+                .await
+                .context(UserNotFoundByIdSnafu { user_id })?
+            }
+            UserInfoRequest::UserInfoRequestByName(req) => {
+                let username = &req.username;
+                let users_url = users_base_url
+                    .join(&format!("?username={username}&exact=true"))
+                    .context(ConstructOidcEndpointPathSnafu)?;
+
+                let users = send_json_request::<Vec<UserMetadata>>(
+                    self.http_client
+                        .get(users_url)
+                        .bearer_auth(&authn.access_token),
+                )
+                .await
+                .context(SearchForUserSnafu)?;
+
+                if users.len() > 1 {
+                    return TooManyUsersReturnedSnafu.fail();
+                }
+
+                users
+                    .first()
+                    .cloned()
+                    .context(UserNotFoundByNameSnafu { username })?
+            }
+        };
+
+        let groups = send_json_request::<Vec<GroupMembership>>(
+            self.http_client
+                .get(
                     users_base_url
-                        .join(&req.id)
+                        .join(&format!("{}/groups", user_info.id))
                         .context(ConstructOidcEndpointPathSnafu)?,
                 )
                 .bearer_auth(&authn.access_token),
-            )
-            .await
-            .context(UserNotFoundByIdSnafu { user_id })?
-        }
-        UserInfoRequest::UserInfoRequestByName(req) => {
-            let username = &req.username;
-            let users_url = users_base_url
-                .join(&format!("?username={username}&exact=true"))
-                .context(ConstructOidcEndpointPathSnafu)?;
-
-            let users = send_json_request::<Vec<UserMetadata>>(
-                http.get(users_url).bearer_auth(&authn.access_token),
-            )
-            .await
-            .context(SearchForUserSnafu)?;
-
-            if users.len() > 1 {
-                return TooManyUsersReturnedSnafu.fail();
-            }
-
-            users
-                .first()
-                .cloned()
-                .context(UserNotFoundByNameSnafu { username })?
-        }
-    };
-
-    let groups = send_json_request::<Vec<GroupMembership>>(
-        http.get(
-            users_base_url
-                .join(&format!("{}/groups", user_info.id))
-                .context(ConstructOidcEndpointPathSnafu)?,
         )
-        .bearer_auth(&authn.access_token),
-    )
-    .await
-    .context(RequestUserGroupsSnafu {
-        username: user_info.username.clone(),
-        user_id: user_info.id.clone(),
-    })?;
+        .await
+        .context(RequestUserGroupsSnafu {
+            username: user_info.username.clone(),
+            user_id: user_info.id.clone(),
+        })?;
 
-    Ok(UserInfo {
-        id: Some(user_info.id),
-        username: Some(user_info.username),
-        groups: groups.into_iter().map(|g| g.path).collect(),
-        custom_attributes: user_info.attributes,
-    })
+        Ok(UserInfo {
+            id: Some(user_info.id),
+            username: Some(user_info.username),
+            groups: groups.into_iter().map(|g| g.path).collect(),
+            custom_attributes: user_info.attributes,
+        })
+    }
 }
